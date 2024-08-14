cea618d3de141cb90c55f87210602860b3a72d7a
rhino-site <rhino.site.github@gmail.com>
2024-08-14 21:58:12 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Rhino Linux Wiki',
   },
 }
 
