2423cec2e090938e9ac612db2231502e2d7efbac
rhino-site <rhino.site.github@gmail.com>
2024-08-14 22:03:59 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Rhino Linux'
+    }
+  },
   logo: <span>Rhino Linux Wiki</span>,
   project: {
     link: 'https://github.com/rhino-linux',
